71aad6714171d7262adcf4d7e6b585cabbe49740
Talha Saeed <m.talha@zixel.cn>
2024-02-01 01:00:30 +0500

    More Navigation added

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -71,7 +71,38 @@ const Navbar = () => {
       "Plug-ins",
       "All Add-ons",
     ],
-    [],
+    ["asd"],
+  ];
+
+  const [moreItemIndex, setMoreItemIndex] = useState(0);
+
+  const moreData = [
+    {
+      title: "CMS Templates",
+      sub_items1: [
+        "Dropal",
+        "Joomla",
+        "Magento",
+        "Muse",
+        "OpenCart",
+        "prestaShop",
+      ],
+      sub_items2: ["Shopify", "Unbounce", "Ghost", "Tumblr"],
+      ending: "All CSS Templates",
+    },
+    {
+      title: "WordPress",
+      sub_items1: ["Themes", "Plugins", "Template Kits"],
+      ending: "All WordPress",
+    },
+    {
+      title: "3D",
+      ending: "All 3D",
+    },
+    {
+      title: "Free Files",
+      ending: "Monthly Free Files",
+    },
   ];
 
   const [activeDropdown, setActiveDropdown] = useState(null);
@@ -119,11 +150,89 @@ const Navbar = () => {
               onMouseLeave={handleNavitemLeave}
             >
               <span>{nav}</span>
-              {activeDropdown === nav && dropdownContent[index].length > 0 && (
-                <div className={styles.dropdown}>
-                  {dropdownContent[index].map((item, idx) => (
-                    <span key={idx}>{item}</span>
-                  ))}
+              {activeDropdown === nav &&
+                dropdownContent[index].length > 0 &&
+                activeDropdown !== "More" && (
+                  <div className={styles.dropdown}>
+                    {dropdownContent[index].map((item, idx) => (
+                      <span key={idx}>{item}</span>
+                    ))}
+                  </div>
+                )}
+              {nav === "More" && activeDropdown === "More" && (
+                <div className={styles.Moredropdown}>
+                  <div className={styles.dropdownLeft}>
+                    <div
+                      onMouseEnter={() => {
+                        setMoreItemIndex(0);
+                      }}
+                      className={styles.arrowCont}
+                    >
+                      <span>CMS Template</span>
+                      <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                    </div>
+                    <div
+                      onMouseEnter={() => {
+                        setMoreItemIndex(1);
+                      }}
+                      className={styles.arrowCont}
+                    >
+                      <span>WordPress</span>
+                      <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                    </div>
+                    <div
+                      onMouseEnter={() => {
+                        setMoreItemIndex(2);
+                      }}
+                      className={styles.arrowCont}
+                    >
+                      <span>3D</span>
+                      <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                    </div>
+                    <div
+                      onMouseEnter={() => {
+                        setMoreItemIndex(3);
+                      }}
+                      className={styles.arrowCont}
+                    >
+                      <span>FreeFIles</span>
+                      <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                  <div className={styles.dropdownRight}>
+                    <p className={styles.dropdownRightHeading}>
+                      {moreData[moreItemIndex].title}
+                    </p>
+                    {moreData[moreItemIndex].sub_items1 &&
+                      moreData[moreItemIndex].sub_items2 && (
+                        <div className={styles.rightCols}>
+                          {moreData[moreItemIndex].sub_items1 && (
+                            <div className={styles.rightColsData}>
+                              {moreData[moreItemIndex].sub_items1.map(
+                                (item, idx) => (
+                                  <span key={idx}>{item}</span>
+                                )
+                              )}
+                            </div>
+                          )}
+                          {moreData[moreItemIndex].sub_items2 && (
+                            <div className={styles.rightColsData}>
+                              {moreData[moreItemIndex].sub_items2.map(
+                                (item, idx) => (
+                                  <span key={idx}>{item}</span>
+                                )
+                              )}
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    <div className={styles.dropdownRightEndingContainer}>
+                      <p className={styles.dropdownRightEnding}>
+                        {moreData[moreItemIndex].ending}
+                      </p>
+                      <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                    </div>
+                  </div>
                 </div>
               )}
             </div>
